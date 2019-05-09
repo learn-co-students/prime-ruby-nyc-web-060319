3ed40bd486061bf28e93425ff3583c3b92ea2e9b
Evan Schechter <schechter.evan@gmail.com>
2019-05-09 17:51:50 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+def prime?(n)
+  if n < 0
+    n = -n
+  end
+  if n <= 1
+	  return false
+  end
+
+  if n == 2
+    return true
+  end
+
+  for i in 2..(n/2)
+    if n % i == 0
+      return false
+    end
+  end
+  return true
+
+end
